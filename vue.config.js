0cb34b11c722324e3078891563d97e2d40b5c2fd
情雨随风 <876451336@qq.com>
2019-06-20 00:07:14 +0800

    添加打包不生成map文件

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,7 @@ module.exports = {
         config.resolve.alias
             .set('@', resolve('src'))
     },
+    productionSourceMap: false,
     devServer: {
         port: 8800,
         proxy: {
