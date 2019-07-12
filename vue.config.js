47d170f0b3e915e0c65697c525d110ff73d3c577
情雨随风 <876451336@qq.com>
2019-07-12 17:10:54 +0800

    配置打包文件合并

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,27 @@
 const path = require('path')
 
 const resolve = (dir) => path.join(__dirname, dir)
+function recursiveIssuer(m) {
+    if (m.issuer) {
+      return recursiveIssuer(m.issuer);
+    } else if (m.name) {
+      return m.name;
+    } else {
+      return false;
+    }
+}
 
 module.exports = {
     chainWebpack: (config) => {
+        const splitOptions = config.optimization.get('splitChunks')
+        splitOptions.cacheGroups.appStyles = {
+            name: 'styles',
+            test: (m, c, entry = 'app') => m.constructor.name === 'CssModule' && recursiveIssuer(m) === entry,
+            chunks: 'all',
+            minChunks: 1,
+            enforce: true
+        }
+        config.optimization.splitChunks(splitOptions)
         config.resolve.alias
             .set('@', resolve('src'))
     },
